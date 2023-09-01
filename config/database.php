cf1a044d94c64e73b03ce4f37a669eb570848f0c
Marie Ramlow <me@nycode.dev>
2023-09-01 18:22:05 +0200

    add unix config support

--- config/database.php
+++ config/database.php
@@ -129,6 +129,7 @@ return [
         'default' => [
             'scheme'   => env('REDIS_SCHEME', 'tcp'),
             'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'path'     => env('REDIS_PATH', null),
             'password' => env('REDIS_PASSWORD', null),
             'port'     => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
@@ -137,6 +138,7 @@ return [
         'cache' => [
             'scheme'   => env('REDIS_SCHEME', 'tcp'),
             'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'path'     => env('REDIS_PATH', null),
             'password' => env('REDIS_PASSWORD', null),
             'port'     => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
